75c5ae38711993ebfafa47cb08d629bcc93e0e38
charmynuesca <31788592+charmynuesca@users.noreply.github.com>
2017-09-23 15:12:44 +0800

    Add files via upload

---
+++ db_users.sql
@@ -0,0 +1,24 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: db_users
+Target Host: localhost
+Target Database: db_users
+Date: 9/23/2017 12:32:25 PM
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for uuser
+-- ----------------------------
+DROP TABLE IF EXISTS `uuser`;
+CREATE TABLE `uuser` (
+  `uname` varchar(256) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `uuser` VALUES ('charmy', '123456');
+INSERT INTO `uuser` VALUES ('charm', '543210');
